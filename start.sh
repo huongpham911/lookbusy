c7d686369c5da49c43817bbfd31f730e9b36a400
Huong Pham (HMD) <thanhquang3799@gmail.com>
2025-09-02 01:27:04 +0700

    update

--- start.sh
+++ start.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
-# Khởi chạy lookbusy với CPU random 14–20% và RAM 14–17%
+# Khởi chạy lookbusy với CPU random 14–20% và RAM 9000–10930 MiB
 
 CORES=$(nproc)
 TOTAL_MEM=$(free -m | awk '/Mem:/ {print $2}')
 
-# Random CPU và RAM %
-CPU_PERCENT=$((RANDOM % 7 + 14))   # 14–20
-RAM_PERCENT=$((RANDOM % 4 + 14))   # 14–17
+# Random CPU %
+CPU_PERCENT=$((RANDOM % 7 + 14))   # 14–20 %
+
+# Random RAM (9000–10930 MiB)
+MEM_TO_USE=$((RANDOM % 1931 + 9000))   # 9000–10930
 
 echo "==== Lookbusy Auto Load Generator ===="
 echo "Detected CPU cores : ${CORES}"
 echo "Total RAM          : ${TOTAL_MEM} MiB"
-echo "Using RAM          : ~${RAM_PERCENT}%"
+echo "Using RAM          : ~${MEM_TO_USE} MiB"
 echo "CPU Util           : ~${CPU_PERCENT}% trên ${CORES} cores"
 echo "======================================"
 
@@ -19,4 +21,4 @@ echo "======================================"
 exec lookbusy \
   --ncpus=${CORES} \
   --cpu-util=${CPU_PERCENT} \
-  --mem-util=${RAM_PERCENT}%
+  --mem-util=${MEM_TO_USE}M
